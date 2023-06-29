cf57a53bf182b1587c004cb2236e9048704316c9
Patrick Michael Chiuco <patrickchiuco@Patricks-MacBook-Pro.local>
2023-06-29 11:43:20 +0800

    Working with existing data part 7.

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,48 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic.edit import DeleteView
+from django.http import Http404
 from .models import Notes
+from .forms import NotesForm
 # Create your views here.
 
+class NotesListView(ListView):
+	model = Notes
+	context_object_name = 'notes' # default is objects
+	template_name = 'notes/notes_list.html' # followed default so no need
+
+class NotesDetailView(DetailView):
+	model = Notes
+	context_object_name = 'note'
+
+class NotesCreateView(CreateView):
+	model = Notes
+	#fields = ['title', 'text']
+	form_class = NotesForm
+	success_url = '/smart/notes/'
+
+class NotesUpdateVIew(UpdateView):
+	model = Notes
+	#fields = ['title', 'text']
+	form_class = NotesForm
+	success_url = '/smart/notes/'
+
+class NotesDeleteView(DeleteView):
+	model = Notes
+	success_url = '/smart/notes/'
+	template_name = 'notes/notes_delete.html'
+
+	
+"""
 def list(request):
+	all_notes = Notes.objects.all()
+	return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+
+def detail(request, pk):
+	try:
+		note = Notes.objects.get(pk=pk)
+		return render(request, 'notes/notes_detail.html', {'note': note})
+	except Notes.DoesNotExist:
+		raise Http404('Note doesn\'t exist')
+"""
